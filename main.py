170f915e05957dc98216deea5b81d068d85e3ce8
helno1234 <156999742+helno1234@users.noreply.github.com>
2024-02-13 13:29:26 +0100

    Basic main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import pygame as pg
 
+# Variabel som styrer om spillet skal kjøres
+run = True
+
+# Spill-løkken
+while run:
+  
+  # Går gjennom henselser (events)
+  for event in pg.event.get():
+      # Sjekker om vi ønsker å lukke vinduet
+      if event.type == pg.QUIT:
+          run = False # Spillet skal avsluttes
+
+pg.quit()
