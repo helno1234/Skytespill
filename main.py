5222b94d8d67072f9a5989bf3f182ac7e4f556d3
helno1234 <156999742+helno1234@users.noreply.github.com>
2024-02-15 08:46:51 +0100

    Oppdaterte til klasse Spillbrett
    Oppdaterte alt slik at alt ligger inne i et spillbrett-objekt. Enklere å håndtere

--- main.py
+++ main.py
@@ -3,40 +3,53 @@ from settings import *
 
 class Spillbrett:
     def __init__(self):
-        pass
+        # Initiere pygame
+        pg.init()
+        
+        # Lager en klokke
+        self.klokke = pg.time.Clock()
+        
+        # Lager en overflate vi kan tegne på
+        self.overflate = pg.display.set_mode(STØRRELSE)
+        
+        # Variabel som styrer om spillet skal kjøres
+        self.kjører = True
 
-
-
-# Initiere pygame
-pg.init()
-
-# Lager en overflate vi kan tegne på
-overflate = pg.display.set_mode(STØRRELSE)
-
-# Lager en klokke
-klokke = pg.time.Clock()
-
-# Variabel som styrer om spillet skal kjøres
-kjør = True
-
-# Henter sky-bildet og endrer størrelsen slik at den passer til skjerm, uavhengig av størrelse
-himmel_bilde = pg.transform.scale(pg.image.load("himmel_skytespill.jpeg"), (BREDDE, HØYDE))
-
-overflate.blit(himmel_bilde, (0, 0))
-
-# Spill-løkken
-while kjør:
+    def ny(self):
+        self.kjør()
     
-    # Sørger for at løkken kjører i korrekt hastighet
-    klokke.tick(FPS)
-  
-    # Går gjennom henselser (events)
-    for hendelse in pg.event.get():
-        # Sjekker om vi ønsker å lukke vinduet
-        if hendelse.type == pg.QUIT:
-            kjør = False # Spillet skal avsluttes
+    def kjør(self):
+        self.spiller = True
+        
+        while self.spiller:
+            self.klokke.tick(FPS)
+            self.hendelser()
+            # self.oppdater()
+            self.tegne()
             
-    # "Flipper" displayet for å vise hva vi har tegnet
-    pg.display.flip()
+    def hendelser(self):       
+        # Går gjennom henselser (events)
+        for hendelse in pg.event.get():
+            # Sjekker om vi ønsker å lukke vinduet
+            if hendelse.type == pg.QUIT:
+                if self.spiller:
+                    self.spiller = False
+                self.kjører = False # Spillet skal avsluttes
+            
+    def tegne(self):
+        # Henter sky-bildet og endrer størrelsen slik at den passer til skjerm, uavhengig av størrelse
+        himmel_bilde = pg.transform.scale(pg.image.load("himmel_skytespill.jpeg"), (BREDDE, HØYDE))
+
+        # Tegner bakgrunnsbildet på skjermen
+        self.overflate.blit(himmel_bilde, (0, 0))
+        
+        # "Flipper" displayet for å vise hva vi har tegnet
+        pg.display.flip()
+
+spillbrett_objekt = Spillbrett()
+
+while spillbrett_objekt.kjører:
+    
+    spillbrett_objekt.ny()
 
 pg.quit()
